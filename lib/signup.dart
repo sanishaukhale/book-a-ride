ae4eadcf2d83f2b050d64e2c724496ce2f9aa984
sanishaukhale <sanishaukhale5@gmail.com>
2023-12-11 19:02:15 +0530

    signup dummy ui

--- lib/signup.dart
+++ lib/signup.dart
@@ -2,10 +2,52 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Signup extends StatelessWidget {
-  const Signup({super.key});
-
+  Signup({super.key});
+  final TextEditingController email = TextEditingController();
+  final TextEditingController password = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("SIGNUP"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(left: 25.0, right: 25.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              "EMAIL",
+              style: TextStyle(fontSize: 15),
+            ),
+            TextFormField(
+              controller: email,
+            ),
+            const Text(
+              "PASSWORD",
+              style: TextStyle(fontSize: 15),
+            ),
+            TextFormField(
+              controller: password,
+            ),
+            const Text(
+              "CONFIRM PASSWORD",
+              style: TextStyle(fontSize: 15),
+            ),
+            TextFormField(),
+            SizedBox(height: 10,),
+            Center(
+              child: ElevatedButton(
+                onPressed: () {},
+                child: const Text(
+                  "SIGNUP",
+                  style: TextStyle(fontSize: 15),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
