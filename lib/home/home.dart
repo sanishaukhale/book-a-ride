b919e93e174d99e7ec6e11b778bfdda16b0e3618
sanishaukhale <sanishaukhale5@gmail.com>
2023-12-13 00:03:57 +0530

    firestore data fetch - able to fetch data - need to display

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -5,9 +6,34 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("HOME PAGE"),
+    return  Scaffold(
+      appBar: AppBar(
+        title: Text('Firestore Data'),
+      ),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection('vehicles').snapshots(),
+        builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return CircularProgressIndicator(); // Loading indicator while fetching data
+          }
+
+          if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
+          }
+
+          // If data is available, build the list
+          List<Widget> children = snapshot.data!.docs.map((DocumentSnapshot document) {
+            Map<String, dynamic> data = document.data() as Map<String, dynamic>;
+            return ListTile(
+              title: Text('User ID: ${document.id}'),
+              subtitle: Text('Data: $data'),
+            );
+          }).toList();
+
+          return ListView(
+            children: children,
+          );
+        },
       ),
     );
   }
