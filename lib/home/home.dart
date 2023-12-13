c9cac185d9a28947c8ef5414265f3fdddf33f68d
sanishaukhale <sanishaukhale5@gmail.com>
2023-12-13 11:26:43 +0530

    home page with car detail complete -Fetch and display from firestore

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,35 +1,65 @@
+import 'package:book_a_ride/details.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: AppBar(
-        title: Text('Firestore Data'),
+        title: const Text('Cars'),
       ),
       body: StreamBuilder(
         stream: FirebaseFirestore.instance.collection('vehicles').snapshots(),
         builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
-            return CircularProgressIndicator(); // Loading indicator while fetching data
+            return const Center(child: CircularProgressIndicator());
           }
-
           if (snapshot.hasError) {
             return Text('Error: ${snapshot.error}');
           }
-
-          // If data is available, build the list
-          List<Widget> children = snapshot.data!.docs.map((DocumentSnapshot document) {
+          List<Widget> children =
+              snapshot.data!.docs.map((DocumentSnapshot document) {
             Map<String, dynamic> data = document.data() as Map<String, dynamic>;
-            return ListTile(
-              title: Text('User ID: ${document.id}'),
-              subtitle: Text('Data: $data'),
+            return GestureDetector(
+              onTap: () {
+                Get.to(const Details());
+              },
+              child: Card(
+                elevation: 4.0,
+                child: Column(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        image: DecorationImage(
+                          image: NetworkImage(data["image"][0]),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      height: 200.0,
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(15.0),
+                      alignment: Alignment.centerLeft,
+                      child: Row(
+                        children: [
+                          Text(data["name"]),
+                          const Spacer(),
+                          Text(data["availability"]
+                              ? "Available"
+                              : "Unavailable"),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             );
           }).toList();
-
           return ListView(
             children: children,
           );
